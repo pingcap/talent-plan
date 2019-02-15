84d0bb3603c65267bf13020a907adf40e54689d2
Neil Shen <overvenus@gmail.com>
2019-02-15 10:46:57 +0800

    raft: add basic structs (#4)

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,14 @@
+#![feature(integer_atomics)]
+
+extern crate labcodec;
+extern crate labrpc;
+extern crate prost;
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate prost_derive;
+extern crate futures;
+extern crate rand;
+
 mod kvraft;
 mod raft;
--- src/raft/mod.rs
+++ src/raft/mod.rs
@@ -1,1 +1,230 @@
+use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
+use std::sync::Arc;
 
+use futures::sync::mpsc::UnboundedSender;
+use labcodec;
+
+pub mod errors;
+pub mod persister;
+pub mod service;
+
+use self::errors::*;
+use self::persister::*;
+use self::service::*;
+
+pub struct ApplyMsg {
+    pub command_valid: bool,
+    pub command: Vec<u8>,
+    pub command_index: u64,
+}
+
+/// State of a raft peer.
+#[derive(Default)]
+pub struct State {
+    /// The current term of this peer.
+    term: AtomicU64,
+    /// Whether this peer believes it is the leader.
+    is_leader: AtomicBool,
+}
+
+impl State {
+    pub fn term(&self) -> u64 {
+        self.term.load(Ordering::Relaxed)
+    }
+
+    pub fn is_leader(&self) -> bool {
+        self.is_leader.load(Ordering::Relaxed)
+    }
+}
+
+// A single Raft peer.
+pub struct Raft {
+    // RPC end points of all peers
+    peers: Vec<RaftClient>,
+    // Object to hold this peer's persisted state
+    persister: Box<dyn Persister>,
+    // this peer's index into peers[]
+    me: usize,
+    state: Arc<State>,
+    // Your data here (2A, 2B, 2C).
+    // Look at the paper's Figure 2 for a description of what
+    // state a Raft server must maintain.
+}
+
+impl Raft {
+    // the service or tester wants to create a Raft server. the ports
+    // of all the Raft servers (including this one) are in peers. this
+    // server's port is peers[me]. all the servers' peers arrays
+    // have the same order. persister is a place for this server to
+    // save its persistent state, and also initially holds the most
+    // recent saved state, if any. apply_ch is a channel on which the
+    // tester or service expects Raft to send ApplyMsg messages.
+    // Make() must return quickly, so it should start goroutines
+    // for any long-running work.
+    pub fn new(
+        peers: Vec<RaftClient>,
+        me: usize,
+        persister: Box<dyn Persister>,
+        apply_ch: UnboundedSender<ApplyMsg>,
+        state: Arc<State>,
+    ) -> Raft {
+        let raft_state = persister.raft_state();
+
+        // Your initialization code here (2A, 2B, 2C).
+        let mut rf = Raft {
+            peers,
+            persister,
+            me,
+            state,
+        };
+
+        // initialize from state persisted before a crash
+        rf.restore(&raft_state);
+
+        rf
+    }
+
+    /// save Raft's persistent state to stable storage,
+    /// where it can later be retrieved after a crash and restart.
+    /// see paper's Figure 2 for a description of what should be persistent.
+    fn persist(&mut self) {
+        // Your code here (2C).
+        // Example:
+        // labcodec::encode(&self.xxx, &mut data).unwrap();
+        // labcodec::encode(&self.yyy, &mut data).unwrap();
+        // self.persister.save_raft_state(data);
+    }
+
+    /// restore previously persisted state.
+    fn restore(&mut self, data: &[u8]) {
+        if data.is_empty() {
+            // bootstrap without any state?
+            return;
+        }
+        // Your code here (2C).
+        // Example:
+        // match labcodec::decode(data) {
+        //     Ok(o) => {
+        //         self.xxx = o.xxx;
+        //         self.yyy = o.yyy;
+        //     }
+        //     Err(e) => {
+        //         panic!("{:?}", e);
+        //     }
+        // }
+    }
+
+    /// example code to send a RequestVote RPC to a server.
+    /// server is the index of the target server in rf.peers[].
+    /// expects RPC arguments in args.
+    /// fills in *reply with RPC reply, so caller should
+    /// pass &reply.
+    /// the types of the args and reply passed to Call() must be
+    /// the same as the types of the arguments declared in the
+    /// handler function (including whether they are pointers).
+    ///
+    /// The labrpc package simulates a lossy network, in which servers
+    /// may be unreachable, and in which requests and replies may be lost.
+    /// Call() sends a request and waits for a reply. If a reply arrives
+    /// within a timeout interval, Call() returns true; otherwise
+    /// Call() returns false. Thus Call() may not return for a while.
+    /// A false return can be caused by a dead server, a live server that
+    /// can't be reached, a lost request, or a lost reply.
+    ///
+    /// Call() is guaranteed to return (perhaps after a delay) *except* if the
+    /// handler function on the server side does not return.  Thus there
+    /// is no need to implement your own timeouts around Call().
+    ///
+    /// look at the comments in ../labrpc/src/mod.rs for more details.
+    ///
+    /// if you're having trouble getting RPC to work, check that you've
+    /// capitalized all field names in structs passed over RPC, and
+    /// that the caller passes the address of the reply struct with &, not
+    /// the struct itself.
+    fn send_request_vote(&self, server: usize, args: &RequestVoteArgs) -> Result<RequestVoteReply> {
+        self.peers[server].request_vote(&args).map_err(Error::Rpc)
+    }
+
+    fn start<M>(&self, command: &M) -> Result<(u64, u64)>
+    where
+        M: labcodec::Message,
+    {
+        let index = 0;
+        let term = 0;
+        let is_leader = true;
+        let mut buf = vec![];
+        labcodec::encode(command, &mut buf).map_err(Error::Encode)?;
+        // Your code here (2B).
+
+        if is_leader {
+            Ok((index, term))
+        } else {
+            Err(Error::NotLeader)
+        }
+    }
+}
+
+// Choose concurrency paradigm.
+//
+// You can either drive the raft state machine by the rpc framework,
+//
+// ```rust
+// struct Node { raft: Arc<Mutex<Raft>> }
+// ```
+//
+// or spawn a new thread runs the raft state machine and communicate via
+// a channel.
+//
+// ```rust
+// struct Node { sender: Sender<Msg> }
+// ```
+#[derive(Clone)]
+pub struct Node {
+    // Your code here.
+}
+
+impl Node {
+    /// Create a new raft service.
+    pub fn new(raft: Raft) -> Node {
+        // Your code here.
+        Node {}
+    }
+
+    /// the service using Raft (e.g. a k/v server) wants to start
+    /// agreement on the next command to be appended to Raft's log. if this
+    /// server isn't the leader, returns false. otherwise start the
+    /// agreement and return immediately. there is no guarantee that this
+    /// command will ever be committed to the Raft log, since the leader
+    /// may fail or lose an election. even if the Raft instance has been killed,
+    /// this function should return gracefully.
+    ///
+    /// the first return value is the index that the command will appear at
+    /// if it's ever committed. the second return value is the current
+    /// term. the third return value is true if this server believes it is
+    /// the leader.
+    pub fn start<M>(&self, command: &M) -> Result<(u64, u64)>
+    where
+        M: labcodec::Message,
+    {
+        // Your code here.
+        // Example:
+        // self.raft.start(command)
+        unimplemented!()
+    }
+
+    /// the tester calls kill() when a Raft instance won't
+    /// be needed again. you are not required to do anything
+    /// in kill(), but it might be convenient to (for example)
+    /// turn off debug output from this instance.
+    pub fn kill(&self) {
+        // Your code here, if desired.
+    }
+}
+
+impl RaftService for Node {
+    // example RequestVote RPC handler.
+    fn request_vote(&self, args: RequestVoteArgs) -> RequestVoteReply {
+        // Your code here (2A, 2B).
+        unimplemented!()
+    }
+}
