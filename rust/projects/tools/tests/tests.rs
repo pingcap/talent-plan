6bb5d8a3f113169a5bad6bdd3face43cbb5b1d00
Yilin Chen <sticnarf@gmail.com>
2019-03-25 18:11:57 +0800

    Add tests and reference implementation.
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>

--- rust/projects/tools/tests/tests.rs
+++ rust/projects/tools/tests/tests.rs
@@ -1,8 +1,43 @@
+use kvs::KvStore;
+
 #[test]
 fn cli_no_args() {}
 
 #[test]
 fn cli_2() {}
 
+// Should get previously stored value
 #[test]
-fn bar() {}
+fn get_stored_value() {
+    let mut store = KvStore::new();
+
+    store.set("key1".to_owned(), "value1".to_owned());
+    store.set("key2".to_owned(), "value2".to_owned());
+
+    assert_eq!(store.get("key1".to_owned()), "value1");
+    assert_eq!(store.get("key2".to_owned()), "value2");
+}
+
+// Should overwrite existent value
+#[test]
+fn overwrite_value() {
+    let mut store = KvStore::new();
+
+    store.set("key1".to_owned(), "value1".to_owned());
+    assert_eq!(store.get("key1".to_owned()), "value1");
+
+    store.set("key1".to_owned(), "value2".to_owned());
+    assert_eq!(store.get("key1".to_owned()), "value2");
+}
+
+// Should panic if getting a non-existent key
+#[test]
+#[should_panic]
+fn get_non_existent_value() {
+    let mut store = KvStore::new();
+
+    store.set("key1".to_owned(), "value1".to_owned());
+    assert_eq!(store.get("key1".to_owned()), "value1");
+
+    let _ = store.get("key2".to_owned());
+}
