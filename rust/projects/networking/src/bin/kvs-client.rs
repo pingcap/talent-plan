c5f6b6a22a079f8943afb7982bdce0abb796b856
Yilin Chen <sticnarf@gmail.com>
2019-04-08 17:44:06 +0800

    Use structopt for kvs-client
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>

--- rust/projects/networking/src/bin/kvs-client.rs
+++ rust/projects/networking/src/bin/kvs-client.rs
@@ -1,60 +1,48 @@
 use clap::{App, AppSettings, Arg, SubCommand};
 use kvs::{KvStore, Result};
 use std::env::current_dir;
+use std::net::SocketAddr;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+    name = "kvs-client",
+    raw(global_settings = "&[\
+                           AppSettings::DisableHelpSubcommand,\
+                           AppSettings::SubcommandRequiredElseHelp,\
+                           AppSettings::VersionlessSubcommands]")
+)]
+struct Opt {
+    #[structopt(subcommand)]
+    command: Command,
+    #[structopt(
+        long,
+        help = "Sets the server address",
+        value_name = "IP:PORT",
+        default_value = "127.0.0.1:4000",
+        parse(try_from_str)
+    )]
+    addr: SocketAddr,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    #[structopt(name = "get", about = "Get the string value of a given string key")]
+    Get {
+        #[structopt(name = "KEY", help = "A string key")]
+        key: String,
+    },
+    #[structopt(name = "set", about = "Set the value of a string key to a string")]
+    Set {
+        #[structopt(name = "KEY", help = "A string key")]
+        key: String,
+        #[structopt(name = "VALUE", help = "The string value of the key")]
+        value: String,
+    },
+}
 
 fn main() -> Result<()> {
-    let server_addr_arg = Arg::with_name("addr")
-        .help("Sets the server address")
-        .long("addr")
-        .value_name("IP:PORT")
-        .default_value("127.0.0.1:4000");
-    let matches = App::new("kvs-client")
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .setting(AppSettings::DisableHelpSubcommand)
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .setting(AppSettings::VersionlessSubcommands)
-        .subcommand(
-            SubCommand::with_name("set")
-                .about("Set the value of a string key to a string")
-                .usage("kvs-client set <KEY> <VALUE> [--addr IP:PORT]")
-                .arg(Arg::with_name("KEY").help("A string key").required(true))
-                .arg(
-                    Arg::with_name("VALUE")
-                        .help("The string value of the key")
-                        .required(true),
-                )
-                .arg(server_addr_arg.clone()),
-        )
-        .subcommand(
-            SubCommand::with_name("get")
-                .about("Get the string value of a given string key")
-                .usage("kvs-client get <KEY> [--addr IP:PORT]")
-                .arg(Arg::with_name("KEY").help("A string key").required(true))
-                .arg(server_addr_arg),
-        )
-        .get_matches();
-
-    match matches.subcommand() {
-        ("set", Some(matches)) => {
-            let key = matches.value_of("KEY").expect("KEY argument missing");
-            let value = matches.value_of("VALUE").expect("VALUE argument missing");
-
-            let mut store = KvStore::open(current_dir()?)?;
-            store.set(key.to_string(), value.to_string())?;
-        }
-        ("get", Some(matches)) => {
-            let key = matches.value_of("KEY").expect("KEY argument missing");
-
-            let mut store = KvStore::open(current_dir()?)?;
-            if let Some(value) = store.get(key.to_string())? {
-                println!("{}", value);
-            } else {
-                println!("Key not found");
-            }
-        }
-        _ => unreachable!(),
-    }
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
     Ok(())
 }
