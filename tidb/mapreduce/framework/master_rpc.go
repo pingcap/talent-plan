a4af49d14317d3e314a48c32df8a23a87579007a
qw4990 <qw4990@163.com>
2019-04-08 14:13:22 +0800

    update ignore

--- tidb/mapreduce/framework/master_rpc.go
+++ tidb/mapreduce/framework/master_rpc.go
@@ -45,7 +45,7 @@ func (mr *Master) startRPCServer() {
 					conn.Close()
 				}()
 			} else {
-				debug("RegistrationServer: accept error", err)
+				debug("RegistrationServer: accept error")
 				break
 			}
 		}
