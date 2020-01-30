6a472584a135eb36e0577b5cf26d5f1b24e3b1e5
yc322 <1393242780@qq.com>
2020-01-30 13:47:58 +0800

    Update Join.go

--- tidb/join/join.go
+++ tidb/join/join.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"github.com/pingcap/tidb/util/mvmap"
+	"strconv"
+	"sync"
+)
+
 // Join accepts a join query of two relations, and returns the sum of
 // relation0.col0 in the final result.
 // Input arguments:
@@ -9,6 +15,54 @@ package main
 //   offset1: offsets of which columns the given relation1 should be joined
 // Output arguments:
 //   sum: sum of relation0.col0 in the final result
+//func Join(f0, f1 string, offset0, offset1 []int) (sum uint64) {
+//	tbl0, tbl1 := readCSVFileIntoTbl(f0), readCSVFileIntoTbl(f1)
+//	if len(tbl0) < len(tbl1) {
+//		hashtable := buildHashTable(tbl0, offset0)
+//		for _, row := range tbl1 {
+//			rowIDs := probe(hashtable, row, offset1)
+//			for _, id := range rowIDs {
+//				v, err := strconv.ParseUint(tbl0[id][0], 10, 64)
+//				if err != nil {
+//					panic("Join panic\n" + err.Error())
+//				}
+//				sum += v
+//			}
+//		}
+//	}
+//	return sum
+//}
+//
+
+var mu sync.RWMutex
+
+// JoinExample performs a simple hash join algorithm.
 func Join(f0, f1 string, offset0, offset1 []int) (sum uint64) {
+	tbl0, tbl1 := readCSVFileIntoTbl(f0), readCSVFileIntoTbl(f1)
+	hashtable := buildHashTable(tbl0, offset0)
+	var wg sync.WaitGroup
+	for _, row := range tbl1 {
+		wg.Add(1)
+		go JoinSum(hashtable, row, tbl0, &sum, offset0, &wg)
+	}
+	for _, _ = range tbl1 {
+		wg.Wait()
+	}
 	return sum
+}
+
+func JoinSum(hashtable *mvmap.MVMap, row []string, tbl0 [][]string, sum *uint64, offset []int, wg *sync.WaitGroup) {
+	rowIDs := probe(hashtable, row, offset)
+	temp := *sum
+	for _, id := range rowIDs {
+		v, err := strconv.ParseUint(tbl0[id][0], 10, 64)
+		if err != nil {
+			panic("JoinExample panic\n" + err.Error())
+		}
+		temp += v
+	}
+	mu.Lock()
+	*sum = *sum + temp
+	defer mu.Unlock()
+	wg.Done()
 }
--- tidb/mapreduce/urltop10.go
+++ tidb/mapreduce/urltop10.go
@@ -1,9 +1,76 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // URLTop10 .
 func URLTop10(nWorkers int) RoundsArgs {
 	// YOUR CODE HERE :)
 	// And don't forget to document your idea.
-	panic("YOUR CODE HERE")
-	return nil
+	var args RoundsArgs
+	args = append(args, RoundArgs{
+		MapFunc:    UrlTop10Map,
+		ReduceFunc: Round1Reduce,
+		NReduce:    nWorkers,
+	})
+
+	//Round 2
+	args = append(args, RoundArgs{
+		MapFunc:    Round2Map,
+		ReduceFunc: UrlTop10Reduce,
+		NReduce:    1,
+	})
+	return args
+}
+
+func UrlTop10Map(filename string, contents string) []KeyValue {
+	urls := strings.Split(contents, "\n")
+	res := make([]KeyValue, 0, len(urls))
+	for _, url := range urls {
+		url = strings.TrimSpace(url)
+		if len(url) == 0 {
+			continue
+		}
+		res = append(res, KeyValue{Key: url})
+	}
+	return res
+}
+
+func Round1Reduce(key string, values []string) string {
+	return fmt.Sprintf("%s %s\n", key, strconv.Itoa(len(values)))
+}
+
+func Round2Map(filename string, contents string) []KeyValue {
+	lines := strings.Split(contents, "\n")
+	kvs := make([]KeyValue, 0, len(lines))
+	for _, l := range lines {
+		kvs = append(kvs, KeyValue{"", l})
+	}
+	return kvs
+}
+
+func UrlTop10Reduce(key string, values []string) string {
+	cnts := make(map[string]int, len(values))
+	for _, kv := range values {
+		v := strings.TrimSpace(kv)
+		if len(v) == 0 {
+			continue
+		}
+		tmp := strings.Split(v, " ")
+		n, err := strconv.Atoi(tmp[1])
+		if err != nil {
+			panic(err)
+		}
+		cnts[tmp[0]] = n
+	}
+	str, count := TopN(cnts, 10)
+	buf := new(bytes.Buffer)
+	for i := range str {
+		fmt.Fprintf(buf, "%s: %d\n", str[i], count[i])
+	}
+	return buf.String()
 }
