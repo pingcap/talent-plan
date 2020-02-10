2a977358ee2c889c2f3ca946e9788ff978fc6366
Hillium <maruruku@stu.csust.edu.cn>
2020-02-10 22:20:13 +0800

    fix: did some tuning so that cargo_test_persist_concurrent_unreliable_3a fail rate < 5%.
    这个问题没法修……也不用修（大概）……

--- dss/raft/src/kvraft/server.rs
+++ dss/raft/src/kvraft/server.rs
@@ -379,7 +379,7 @@ impl KvStateMachine {
                             .store(message.command_index as usize, Ordering::SeqCst);
                         if let Some(max) = fsm.max_size {
                             // if the log lost behind too much, don't snapshot it, just hurry up to crash the log.
-                            if fsm.raft.log_size() > (max as f64) as usize
+                            if fsm.raft.log_size() > (max as f64 * 0.9) as usize
                                 && diff_idx < Ord::max(1, (max / 50) as u64)
                             {
                                 let last_index = fsm.last_index.load(Ordering::SeqCst);
